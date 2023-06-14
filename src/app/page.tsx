d526d68e00068207a661584e429407d8451e98dc
Andrew Heim <andrewsheim@gmail.com>
2023-06-13 18:18:02 -0600

    testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <main>
       <h1 className="flex justify-center m-5 p-2 font-bold text-4xl">Shortcut Racer</h1>
+      <div className="flex justify-center">
+        Hi Charlie
+      </div>
     </main>
   )
 }
