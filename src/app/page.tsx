d4a9c4529f6b04ab1f7ea2baa7fc0f265e523875
Andrew Heim <andrewsheim@gmail.com>
2023-06-13 18:14:05 -0600

    inital setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="flex justify-center m-5 p-2 font-bold text-4xl">Shortcut Racer</h1>
+    </main>
+  )
+}
